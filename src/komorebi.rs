6cdf4e3fbbbed2494b937675e9802ac278f15639
amrbashir <github@amrbashir.me>
2025-02-12 21:35:06 +0200

    refactor: use in-house komorebi client

--- src/komorebi.rs
+++ src/komorebi.rs
@@ -1,10 +1,171 @@
-use std::io::{BufRead, BufReader};
+use std::io::{BufRead, BufReader, Read, Write};
 
 use anyhow::Context;
-use komorebi_client::{Notification, NotificationEvent, SocketMessage, State};
+use serde::{Deserialize, Serialize};
+use uds_windows::{UnixListener, UnixStream};
 use winit::event_loop::EventLoopProxy;
 
 use crate::app::AppMessage;
+
+#[derive(Debug, Deserialize)]
+struct KWorkspace {
+    name: Option<String>,
+    containers: Ring<serde_json::Value>,
+    maximized_window: Option<serde_json::Value>,
+    monocle_container: Option<serde_json::Value>,
+    floating_windows: Vec<serde_json::Value>,
+}
+
+impl KWorkspace {
+    fn is_empty(&self) -> bool {
+        self.containers.is_empty()
+            && self.maximized_window.is_none()
+            && self.monocle_container.is_none()
+            && self.floating_windows.is_empty()
+    }
+}
+
+#[derive(Debug, Deserialize)]
+struct Ring<T> {
+    elements: Vec<T>,
+    focused: usize,
+}
+
+impl<T> Ring<T> {
+    fn focused(&self) -> Option<&T> {
+        self.elements.get(self.focused)
+    }
+
+    fn focused_idx(&self) -> usize {
+        self.focused
+    }
+
+    fn is_empty(&self) -> bool {
+        self.elements.is_empty()
+    }
+
+    fn iter(&self) -> std::slice::Iter<T> {
+        self.elements.iter()
+    }
+}
+
+#[derive(Debug, Deserialize)]
+struct KMonitor {
+    workspaces: Ring<KWorkspace>,
+}
+
+#[derive(Debug, Deserialize)]
+struct KState {
+    monitors: Ring<KMonitor>,
+}
+
+#[derive(Debug, strum::Display, Serialize, Deserialize)]
+#[serde(tag = "type", content = "content")]
+enum KSocketMessage {
+    State,
+    AddSubscriberSocket(String),
+    FocusWorkspaceNumber(usize),
+}
+
+#[derive(Debug, strum::Display, Serialize, Deserialize)]
+#[serde(tag = "type")]
+enum KSocketEvent {
+    FocusWorkspaceNumber,
+    FocusMonitorNumber,
+    FocusMonitorWorkspaceNumber,
+    FocusNamedWorkspace,
+    FocusWorkspaceNumbers,
+    CycleFocusMonitor,
+    CycleFocusWorkspace,
+    ReloadConfiguration,
+    ReplaceConfiguration,
+    CompleteConfiguration,
+    ReloadStaticConfiguration,
+    MoveContainerToMonitorNumber,
+    MoveContainerToMonitorWorkspaceNumber,
+    MoveContainerToNamedWorkspace,
+    MoveContainerToWorkspaceNumber,
+    MoveWorkspaceToMonitorNumber,
+    CycleMoveContainerToMonitor,
+    CycleMoveContainerToWorkspace,
+    CycleMoveWorkspaceToMonitor,
+    CloseWorkspace,
+    SendContainerToMonitorNumber,
+    SendContainerToMonitorWorkspaceNumber,
+    SendContainerToNamedWorkspace,
+    SendContainerToWorkspaceNumber,
+    CycleSendContainerToMonitor,
+    CycleSendContainerToWorkspace,
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(untagged)]
+enum KNotificationEvent {
+    #[allow(unused)]
+    Socket(KSocketEvent),
+}
+
+#[derive(Debug, Deserialize)]
+struct KNotification {
+    #[allow(unused)]
+    event: KNotificationEvent,
+    state: KState,
+}
+
+const KOMOREBI_SOCK: &str = "komorebi.sock";
+
+fn send_message(message: &KSocketMessage) -> std::io::Result<()> {
+    let socket = dirs::data_local_dir()
+        .expect("there is no local data directory")
+        .join("komorebi")
+        .join(KOMOREBI_SOCK);
+
+    let mut stream = UnixStream::connect(socket)?;
+    stream.set_write_timeout(Some(std::time::Duration::from_secs(1)))?;
+    stream.write_all(serde_json::to_string(message)?.as_bytes())
+}
+
+fn send_query(message: &KSocketMessage) -> std::io::Result<String> {
+    let socket = dirs::data_local_dir()
+        .expect("there is no local data directory")
+        .join("komorebi")
+        .join(KOMOREBI_SOCK);
+
+    let mut stream = UnixStream::connect(socket)?;
+    stream.set_read_timeout(Some(std::time::Duration::from_secs(1)))?;
+    stream.set_write_timeout(Some(std::time::Duration::from_secs(1)))?;
+    stream.write_all(serde_json::to_string(message)?.as_bytes())?;
+    stream.shutdown(std::net::Shutdown::Write)?;
+
+    let mut reader = BufReader::new(stream);
+    let mut response = String::new();
+    reader.read_to_string(&mut response)?;
+
+    Ok(response)
+}
+
+fn subscribe(name: &str) -> std::io::Result<UnixListener> {
+    let socket = dirs::data_local_dir()
+        .expect("there is no local data directory")
+        .join("komorebi")
+        .join(name);
+
+    match std::fs::remove_file(&socket) {
+        Ok(()) => {}
+        Err(error) => match error.kind() {
+            std::io::ErrorKind::NotFound => {}
+            _ => {
+                return Err(error);
+            }
+        },
+    };
+
+    let listener = UnixListener::bind(&socket)?;
+
+    send_message(&KSocketMessage::AddSubscriberSocket(name.to_string()))?;
+
+    Ok(listener)
+}
 
 #[derive(Debug, Clone)]
 pub struct Workspace {
@@ -14,19 +175,18 @@ pub struct Workspace {
     pub is_empty: bool,
 }
 
-fn workspaces_from_state(state: State) -> anyhow::Result<Vec<Workspace>> {
+fn workspaces_from_state(state: KState) -> anyhow::Result<Vec<Workspace>> {
     let monitor = state.monitors.focused().context("No focused monintor")?;
 
-    let focused_workspace = monitor.focused_workspace_idx();
+    let focused_workspace = monitor.workspaces.focused_idx();
 
-    let workspaces = monitor.workspaces().iter().enumerate().map(|(idx, w)| {
-        let name = w.name().clone().unwrap_or_else(|| (idx + 1).to_string());
-        let focused = focused_workspace == idx;
+    let workspaces = monitor.workspaces.iter().enumerate().map(|(idx, w)| {
+        let name = w.name.clone().unwrap_or_else(|| (idx + 1).to_string());
 
         Workspace {
             name,
             idx,
-            focused,
+            focused: focused_workspace == idx,
             is_empty: w.is_empty(),
         }
     });
@@ -35,21 +195,20 @@ fn workspaces_from_state(state: State) -> anyhow::Result<Vec<Workspace>> {
 }
 
 pub fn read_workspaces() -> anyhow::Result<Vec<Workspace>> {
-    let response = komorebi_client::send_query(&SocketMessage::State)?;
-    let state: State = serde_json::from_str(&response)?;
+    let response = send_query(&KSocketMessage::State)?;
+    let state: KState = serde_json::from_str(&response)?;
     workspaces_from_state(state)
 }
 
 pub fn change_workspace(idx: usize) -> anyhow::Result<()> {
-    komorebi_client::send_query(&SocketMessage::FocusWorkspaceNumber(idx))?;
-    Ok(())
+    send_message(&KSocketMessage::FocusWorkspaceNumber(idx)).map_err(Into::into)
 }
 
 const SOCK_NAME: &str = "komorebi-switcher.sock";
 
 pub fn listen_for_workspaces(proxy: EventLoopProxy<AppMessage>) {
     let socket = loop {
-        match komorebi_client::subscribe(SOCK_NAME) {
+        match subscribe(SOCK_NAME) {
             Ok(socket) => break socket,
             Err(_) => std::thread::sleep(std::time::Duration::from_secs(1)),
         };
@@ -59,51 +218,13 @@ pub fn listen_for_workspaces(proxy: EventLoopProxy<AppMessage>) {
         let reader = BufReader::new(incoming);
 
         for line in reader.lines().map_while(Result::ok) {
-            let Ok(notification) = serde_json::from_str::<Notification>(&line) else {
+            let Ok(notification) = serde_json::from_str::<KNotification>(&line) else {
                 continue;
             };
 
-            match notification.event {
-                NotificationEvent::Socket(message) if should_update(&message) => {
-                    if let Ok(new_workspaces) = workspaces_from_state(notification.state) {
-                        let _ = proxy.send_event(AppMessage::UpdateWorkspaces(new_workspaces));
-                    }
-                }
-                _ => {}
+            if let Ok(new_workspaces) = workspaces_from_state(notification.state) {
+                let _ = proxy.send_event(AppMessage::UpdateWorkspaces(new_workspaces));
             }
         }
     }
-}
-
-fn should_update(message: &SocketMessage) -> bool {
-    matches!(
-        message,
-        SocketMessage::FocusLastWorkspace
-            | SocketMessage::FocusMonitorNumber(_)
-            | SocketMessage::FocusMonitorWorkspaceNumber(_, _)
-            | SocketMessage::FocusNamedWorkspace(_)
-            | SocketMessage::FocusWorkspaceNumber(_)
-            | SocketMessage::FocusWorkspaceNumbers(_)
-            | SocketMessage::CycleFocusMonitor(_)
-            | SocketMessage::CycleFocusWorkspace(_)
-            | SocketMessage::ReloadConfiguration
-            | SocketMessage::ReplaceConfiguration(_)
-            | SocketMessage::CompleteConfiguration
-            | SocketMessage::ReloadStaticConfiguration(_)
-            | SocketMessage::MoveContainerToMonitorNumber(_)
-            | SocketMessage::MoveContainerToMonitorWorkspaceNumber(_, _)
-            | SocketMessage::MoveContainerToNamedWorkspace(_)
-            | SocketMessage::MoveContainerToWorkspaceNumber(_)
-            | SocketMessage::MoveWorkspaceToMonitorNumber(_)
-            | SocketMessage::CycleMoveContainerToMonitor(_)
-            | SocketMessage::CycleMoveContainerToWorkspace(_)
-            | SocketMessage::CycleMoveWorkspaceToMonitor(_)
-            | SocketMessage::CloseWorkspace
-            | SocketMessage::SendContainerToMonitorNumber(_)
-            | SocketMessage::SendContainerToMonitorWorkspaceNumber(_, _)
-            | SocketMessage::SendContainerToNamedWorkspace(_)
-            | SocketMessage::SendContainerToWorkspaceNumber(_)
-            | SocketMessage::CycleSendContainerToMonitor(_)
-            | SocketMessage::CycleSendContainerToWorkspace(_)
-    )
 }
