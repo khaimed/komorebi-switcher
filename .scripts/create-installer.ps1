0f0cf6ab4c24a5e8785f6ed1a30f02ed48b0af01
amrbashir <github@amrbashir.me>
2025-03-17 10:26:33 +0200

    chore: consistent path in create-installer.ps1

--- .scripts/create-installer.ps1
+++ .scripts/create-installer.ps1
@@ -6,13 +6,13 @@ if (!(Test-Path $exe)) {
   cargo build --release
 }
 
-Copy-Item -Force $exe "installer/komorebi-switcher.exe"
+Copy-Item -Force $exe "./installer/komorebi-switcher.exe"
 
-makensis /V4 "installer/installer.nsi"
+makensis /V4 "./installer/installer.nsi"
 
 New-Item -Force "dist" -Type Directory > $null
 
-Move-Item -Force "installer/komorebi-switcher.exe" "dist/komorebi-switcher.exe"
-Move-Item -Force "installer/komorebi-switcher-setup.exe" "dist/komorebi-switcher-setup.exe"
+Move-Item -Force "./installer/komorebi-switcher.exe" "./dist/komorebi-switcher.exe"
+Move-Item -Force "./installer/komorebi-switcher-setup.exe" "./dist/komorebi-switcher-setup.exe"
 
-Compress-Archive -Update "dist/komorebi-switcher.exe" "dist/komorebi-switcher.zip"
+Compress-Archive -Update "./dist/komorebi-switcher.exe" "./dist/komorebi-switcher.zip"
