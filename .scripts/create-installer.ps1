aa8b61e633d2165117e4cb40193708dfefe9f380
amrbashir <github@amrbashir.me>
2025-03-17 08:41:25 +0200

    ci: add create-installer.ps1 script

---
+++ .scripts/create-installer.ps1
@@ -0,0 +1,18 @@
+$targetDir = if ($env:CARGO_TARGET_DIR) { $env:CARGO_TARGET_DIR } else { './target' }
+
+$exe = "$targetDir/release/komorebi-switcher.exe"
+
+if (!(Test-Path $exe)) {
+  cargo build --release
+}
+
+Copy-Item -Force $exe "installer/komorebi-switcher.exe"
+
+makensis /V4 "installer/installer.nsi"
+
+New-Item -Force "dist" -Type Directory > $null
+
+Move-Item -Force "installer/komorebi-switcher.exe" "dist/komorebi-switcher.exe"
+Move-Item -Force "installer/komorebi-switcher-setup.exe" "dist/komorebi-switcher-setup.exe"
+
+Compress-Archive "dist/komorebi-switcher.exe" "dist/komorebi-switcher.zip"
