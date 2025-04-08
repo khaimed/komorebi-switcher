6fa8b87c2a7183c52afe1063f0e442374b8fad85
amrbashir <github@amrbashir.me>
2025-04-08 06:32:41 +0200

    chore: update lock file in release script

--- .scripts/release.ps1
+++ .scripts/release.ps1
@@ -3,14 +3,14 @@ $version = $args[0];
 $path = "Cargo.toml"
 (Get-Content $path) -replace "version = `"[0-9]+.[0-9]+.[0-9]+`"", "version = `"$version`"" | Set-Content $path
 
+cargo update -p komorebi-switcher # update the lock file
+
 $path = "installer/installer.nsi"
 (Get-Content $path) -replace "VERSION `"[0-9]+.[0-9]+.[0-9]+`"", "VERSION `"$version`"" | Set-Content $path
 
 $path = "CHANGELOG.md"
 $date = Get-Date -Format "yyyy-MM-dd"
 (Get-Content $path) -replace "## \[Unreleased\]", "## [Unreleased]`n`n## [$version] - $date" | Set-Content $path
-
-Start-Sleep -Seconds 2
 
 git add .
 git commit -m "release: v$version";
